454aa387e86c8869b2ae846269ba1c8eeeb0a8e9
정재한 <wogks27@simbaat.com>
2023-02-16 16:17:17 +0900

    Feat: navigationbar material design 3

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -37,45 +37,25 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: screens[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        //네비게이션바 색깔변경 강제
-        type: BottomNavigationBarType.shifting,
-        currentIndex: _currentIndex,
-        onTap: _onTap,
-        selectedItemColor: Theme.of(context).primaryColor,
-        items: const [
-          BottomNavigationBarItem(
-            tooltip: 'what are u',
-            label: 'home',
-            icon: FaIcon(FontAwesomeIcons.house),
-            backgroundColor: Colors.amber
-          ),
-          BottomNavigationBarItem(
-            label: 'search',
-            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
-            backgroundColor: Colors.blue
-          ),
-          BottomNavigationBarItem(
-            tooltip: 'what are u',
-            label: 'home',
-            icon: FaIcon(FontAwesomeIcons.house),
-            backgroundColor: Colors.pink
-          ),
-          BottomNavigationBarItem(
-            label: 'search',
-            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
-            backgroundColor: Colors.yellow
-          ),
-          BottomNavigationBarItem(
-            tooltip: 'what are u',
-            label: 'home',
-            icon: FaIcon(FontAwesomeIcons.house),
-            backgroundColor: Colors.teal
-          ),
-          
-        ],
-      ),
-    );
+        body: screens[_currentIndex],
+        bottomNavigationBar:
+            //마테리얼디자인3
+            NavigationBar(
+          labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+          selectedIndex: _currentIndex,
+          onDestinationSelected: _onTap,
+          destinations: const [
+            NavigationDestination(
+                icon: FaIcon(
+                  FontAwesomeIcons.house,
+                  color: Colors.teal,
+                ),
+                label: 'home'),
+            NavigationDestination(
+                icon: FaIcon(FontAwesomeIcons.magnifyingGlass,
+                    color: Colors.amber),
+                label: 'search'),
+          ],
+        ));
   }
 }
