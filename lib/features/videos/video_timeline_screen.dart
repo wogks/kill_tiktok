25fb890489341b7c73563679f7f3a9291b25c28b
wogks <wogks27@naver.com>
2023-02-23 00:06:55 +0900

    Feat: refreshindicator

--- lib/features/videos/video_timeline_screen.dart
+++ lib/features/videos/video_timeline_screen.dart
@@ -30,6 +30,7 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
   void _onVideoFinished() {
     return;
     //다음페이지로 넘어가틑 메서드
+    // ignore: dead_code
     _pageController.nextPage(
       duration: _scrollDuration,
       curve: _scrollCurve,
@@ -42,18 +43,32 @@ class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
     super.dispose();
   }
 
+  Future<void> _onRefresh()async{
+    //원래 api자리
+    return Future.delayed(const Duration(seconds: 2));
+
+  }
+
   @override
   Widget build(BuildContext context) {
     //이미 부의 scaffold 위젯 안에 있어서 또  할필요 없음
-    return PageView.builder(
-        //자석효과 없어짐
-        //pageSnapping: false,
-        controller: _pageController,
-        onPageChanged: _onPageChanged,
-        itemCount: _itemCount,
-        scrollDirection: Axis.vertical,
-        itemBuilder: (context, index) =>
-            VideoPost(onVideoFinished: _onVideoFinished,
-            index: index),);
+    return RefreshIndicator(
+      //당겨서 새로고침 기능 반드시 퓨처를 반환해야한다
+      onRefresh: _onRefresh,
+      //위치조정
+      displacement: 40,
+      edgeOffset: 10,
+      color: Theme.of(context).primaryColor,
+      child: PageView.builder(
+          //자석효과 없어짐
+          //pageSnapping: false,
+          controller: _pageController,
+          onPageChanged: _onPageChanged,
+          itemCount: _itemCount,
+          scrollDirection: Axis.vertical,
+          itemBuilder: (context, index) =>
+              VideoPost(onVideoFinished: _onVideoFinished,
+              index: index),),
+    );
   }
 }
