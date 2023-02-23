d37a9af3a9a885e5fac7dceace5eb93d83404910
wogks <wogks27@naver.com>
2023-02-23 23:07:24 +0900

    Feat: comments

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:kill_tiktok/constants/gaps.dart';
 import 'package:kill_tiktok/constants/sizes.dart';
 
 class VideoComments extends StatefulWidget {
@@ -20,6 +21,8 @@ class _VideoCommentsState extends State<VideoComments> {
   Widget build(BuildContext context) {
     //리스트뷰ㅡ 이것저것 들어가야되기 때문에 스캐폴드를 넣는다
     return Container(
+      padding: const EdgeInsets.symmetric(
+          horizontal: Sizes.size16, vertical: Sizes.size10),
       clipBehavior: Clip.hardEdge,
       decoration:
           BoxDecoration(borderRadius: BorderRadius.circular(Sizes.size14)),
@@ -36,10 +39,44 @@ class _VideoCommentsState extends State<VideoComments> {
             ),
           ],
         ),
-        body: ListView.builder(
+        body: ListView.separated(
+          separatorBuilder: (context, index) => Gaps.v20,
           itemCount: 10,
-          itemBuilder: (context, index) => Container(
-            child: const Text('asdasdasd'),
+          itemBuilder: (context, index) => Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const CircleAvatar(
+                radius: 18,
+                child: Text('JH'),
+              ),
+              Gaps.h10,
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'james cheong',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.grey.shade500),
+                    ),
+                    const Text(
+                        'as;djf asdfbewfasdfbewfasdfbewfasdfbewfasdfbewfasdfbewf sadf wef '),
+                  ],
+                ),
+              ),
+              Gaps.h10,
+              Column(
+                children: [
+                  FaIcon(FontAwesomeIcons.heart, color: Colors.grey.shade500),
+                  Gaps.v2,
+                  Text(
+                    '90.2K',
+                    style: TextStyle(color: Colors.grey.shade500),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
