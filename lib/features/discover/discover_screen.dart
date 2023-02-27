7d824f97434ad47a64e9944c89632a884c8a5423
정재한 <wogks27@simbaat.com>
2023-02-27 10:40:09 +0900

    Feat: grid item

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:kill_tiktok/constants/gaps.dart';
 import 'package:kill_tiktok/constants/sizes.dart';
 
 final tabs = [
@@ -51,13 +53,64 @@ class DiscoverScreen extends StatelessWidget {
                 //간격은 얼마나 둘것인지
                 crossAxisSpacing: Sizes.size10,
                 mainAxisSpacing: Sizes.size10,
-                childAspectRatio: 9 / 16,
+                childAspectRatio: 9 / 21,
               ),
-              itemBuilder: (context, index) => Container(
-                  color: Colors.teal,
-                  child: Center(
-                    child: Text('$index'),
-                  )),
+              itemBuilder: (context, index) => Column(
+                children: [
+                  AspectRatio(
+                    aspectRatio: 9 / 16,
+                    child: FadeInImage.assetNetwork(
+                      placeholder: 'assets/images/1.jpeg',
+                      image:
+                          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-17g4GD0tz2O509zGFRQraRj4M46WG_XOqA&usqp=CAU',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Gaps.v10,
+                  const Text(
+                    'this is very long captionthis is very long captionthis is very long captionthis is ',
+                    style: TextStyle(
+                        fontSize: Sizes.size16 + Sizes.size2,
+                        fontWeight: FontWeight.bold),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Gaps.v8,
+                  //아래에있는 모든 텍스트가 같은 스타일로 들어간다
+                  DefaultTextStyle(
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey.shade600),
+                    child: Row(
+                      children: [
+                        const CircleAvatar(
+                          radius: 12,
+                          backgroundImage: NetworkImage(
+                              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqQkJWuqp6vsgOKFQXjzfT3tWrOTlYXTkvYA&usqp=CAU'),
+                        ),
+                        Gaps.h4,
+                        const Expanded(
+                          child: Text(
+                            'my avatar is goint to asnd vew',
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        Gaps.h4,
+                        FaIcon(
+                          FontAwesomeIcons.heart,
+                          size: Sizes.size16,
+                          color: Colors.grey.shade600,
+                        ),
+                        Gaps.h2,
+                        const Text(
+                          '2.5M',
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
             for (var tab in tabs.skip(1))
               Center(
