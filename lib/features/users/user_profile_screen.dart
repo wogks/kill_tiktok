675bc2fd6aa62c1e4a308649c745826ef51b66c5
정재한 <wogks27@simbaat.com>
2023-03-02 13:05:04 +0900

    Feat: sliver appbar

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -15,8 +15,11 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
       slivers: [
         //앱바같지만 스크롤이 가능하다
         SliverAppBar(
-          //밑으로 당기면 늘어난다
+          //살짝만 내려도 앱바 전체가 내려온다 쿵!
+          snap: true,
+          //밑으로 당기면 앱바가 보임
           floating: true,
+          //밑으로 당기면 늘어난다
           stretch: true,
           //앱바를 고정한다
           //pinned: true,
@@ -36,6 +39,21 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             ),
             title: const Text('hello'),
           ),
+        ),
+        SliverFixedExtentList(
+          delegate: SliverChildBuilderDelegate(
+            //아이템카운트 느낌
+            childCount: 50,
+            (context, index) => Container(
+              color: Colors.amber[100 * (index % 9)],
+              child: Align(
+                alignment: Alignment.center,
+                child: Text('$index'),
+              ),
+            ),
+          ),
+          //유닛들의 높이
+          itemExtent: 100,
         ),
       ],
     );
