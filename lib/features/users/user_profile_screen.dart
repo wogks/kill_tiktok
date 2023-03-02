b58159e17c1ee7bfefbee688e4c66267ea1fcbdd
정재한 <wogks27@simbaat.com>
2023-03-02 13:47:42 +0900

    Feat: SliverPersistantHeader

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -15,6 +15,7 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
       //스크롤뷰의 일환
       slivers: [
         //앱바같지만 스크롤이 가능하다
+
         SliverAppBar(
           //살짝만 내려도 앱바 전체가 내려온다 쿵!
           //  snap: true,
@@ -41,6 +42,17 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             title: const Text('hello'),
           ),
         ),
+        //일반적인 플러터 위젯을 sliver안에 넣을때 사용할떄 이걸 쓴다
+        SliverToBoxAdapter(
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 25,
+                backgroundColor: Colors.red,
+              )
+            ],
+          ),
+        ),
         SliverFixedExtentList(
           delegate: SliverChildBuilderDelegate(
             //아이템카운트 느낌
@@ -55,6 +67,10 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
           ),
           //유닛들의 높이
           itemExtent: 100,
+        ),
+        SliverPersistentHeader(
+          pinned: true,
+          delegate: CustomDelegate(),
         ),
         SliverGrid(
           delegate: SliverChildBuilderDelegate(
@@ -78,5 +94,39 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
         ),
       ],
     );
+  }
+}
+
+class CustomDelegate extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      color: Colors.indigo,
+      //부모로부터 최대한 많은 공간을 차지한다
+      child: const FractionallySizedBox(
+        // 1=모든부분 차지
+        heightFactor: 1,
+        child: Center(
+          child: Text(
+            'title!!',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  //보통때 크기
+  double get maxExtent => 100;
+
+  @override
+  //화면 내려갔을때 위에 고정되는 크기
+  double get minExtent => 80;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
   }
 }
