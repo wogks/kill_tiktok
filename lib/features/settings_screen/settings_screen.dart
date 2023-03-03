6add578c77b5f8b281c2832905b22ce2a6c78261
wogks <wogks27@naver.com>
2023-03-04 00:41:14 +0900

    Feat: switchdialog

--- lib/features/settings_screen/settings_screen.dart
+++ lib/features/settings_screen/settings_screen.dart
@@ -1,7 +1,24 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _notifications = false;
+
+  void _onNoificationsChanged(bool? newValue) {
+    if (newValue == null) return;
+    setState(() {
+      _notifications = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +28,19 @@ class SettingsScreen extends StatelessWidget {
       ),
       body: ListView(
         children: [
+          SwitchListTile(value: _notifications, onChanged: _onNoificationsChanged,
+          title: const Text('r가능하면 섭타이틀이랑 이거써라'),),
+          //Switch.adaptive 폰에따라 바뀜
+          CupertinoSwitch(value: _notifications, onChanged: _onNoificationsChanged),
+          Checkbox(value: _notifications, onChanged: _onNoificationsChanged),
+          Switch(value: _notifications, onChanged: _onNoificationsChanged),
+          CheckboxListTile(
+            checkColor: Colors.amber,
+            activeColor: Colors.black,
+            value: _notifications,
+            onChanged: _onNoificationsChanged,
+            title: const Text('Enable notifications'),
+          ),
           ListTile(
             onTap: () async {
               final date = await showDatePicker(
@@ -20,6 +50,7 @@ class SettingsScreen extends StatelessWidget {
                 lastDate: DateTime(2030),
               );
 
+              // ignore: use_build_context_synchronously
               final time = await showTimePicker(
                 context: context,
                 initialTime: TimeOfDay.now(),
@@ -27,26 +58,27 @@ class SettingsScreen extends StatelessWidget {
               print(date);
               print(time);
 
+              // ignore: use_build_context_synchronously
               final booking = await showDateRangePicker(
                 context: context,
                 firstDate: DateTime(1980),
                 lastDate: DateTime(2030),
                 builder: (context, child) {
                   return Theme(
-                    child: child!,
                     data: ThemeData(
-                        appBarTheme: AppBarTheme(
+                        appBarTheme: const AppBarTheme(
                       foregroundColor: Colors.white,
                       backgroundColor: Colors.black,
                     )),
+                    child: child!,
                   );
                 },
               );
               print(booking);
             },
-            title: Text('What is your birthday?'),
+            title: const Text('What is your birthday?'),
           ),
-          AboutListTile()
+          const AboutListTile()
         ],
       ),
     );
