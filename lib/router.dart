a0fd3fbd973ef2cfa5229918a02c79dc04bff10b
wogks <wogks27@naver.com>
2023-03-20 00:11:24 +0900

    Feat: install

--- lib/router.dart
+++ lib/router.dart
@@ -1,57 +1,11 @@
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:kill_tiktok/features/authentication/email_screen.dart';
-import 'package:kill_tiktok/features/authentication/login_screen.dart';
-import 'package:kill_tiktok/features/authentication/sign_up_screen.dart';
-import 'package:kill_tiktok/features/authentication/user_name_screen.dart';
-import 'package:kill_tiktok/features/users/user_profile_screen.dart';
+import 'package:kill_tiktok/features/videos/video_recording_screen.dart';
 
 final router = GoRouter(
   routes: [
     GoRoute(
-        name: SignUpScreen.routeName,
-        path: SignUpScreen.routeURL,
-        builder: (context, state) => const SignUpScreen(),
-        routes: [
-          GoRoute(
-            path: UsernameScreen.routeURL,
-            name: UsernameScreen.routeName,
-            builder: (context, state) => const UsernameScreen(),
-          ),
-          GoRoute(
-            name: EmailScreen.routeName,
-        path: EmailScreen.routeURL,
-        builder: (context, state) {
-          final args = state.extra as EmailScreenArgs;
-          return EmailScreen(username: args.username);
-        }),
-        ]),
-    // GoRoute(
-    //     path: LoginScreen.routeName,
-    //     builder: (context, state) => const LoginScreen()),
-
-
-    // GoRoute(
-    //   name: 'username_screen',
-    //     path: UsernameScreen.routeName,
-    //     pageBuilder: (context, state) {
-    //       return CustomTransitionPage(
-    //         child: const UsernameScreen(),
-    //         transitionsBuilder:
-    //             (context, animation, secondaryAnimation, child) {
-    //           return FadeTransition(opacity: animation, child: ScaleTransition(scale: animation,child: child,),);
-    //         },
-    //       );
-    //     }),
-    
-    GoRoute(
-      path: '/users/:username',
-      builder: (context, state) {
-        final username = state.params['username'];
-        final tab = state.queryParams['show'];
-        print(state.params);
-        return UserProfileScreen(username: username!, tab: tab!);
-      },
+      path: '/',
+      builder: (context, state) => const VideoRecordingScreen(),
     )
   ],
 );
